5bab73b1e1da5827d87f36a9346ac1c8329f5e48
Alden Derf <aldenderfc.fabro99@gmail.com>
2025-07-29 15:09:51 +0800

    Update AuthController.php
    Added methods:
    - register
    - login
    - logout
    -user

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,5 +7,55 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => bcrypt($validated['password']),
+        ]);
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('api-token')-plainTextToken,
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('api-token')->plainTextToken,
+        ]);
+    }
+
+    public function logout(Request $request) {
+        $request->user()->tokens()->delete();
+
+        return response()->json(['message' => 'Logged out']);
+    }
+
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
 }
